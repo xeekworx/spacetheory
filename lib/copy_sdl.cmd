a7a291880a14cdc1d76bf7fd28aa415adaa8337d
Xeek <xeek@xeekworx.com>
2017-04-23 11:19:16 -0500

    Improved the copy_sdl script so that it gives better feedback. It also now copies all libraries into Release\ and Debug\ instead of Release [Static]\ and Debug [Static]\ based on a input to the script. I probably need to add a command-line argument to override the input in case this script is ever used by Visual Studio projects.

--- lib/copy_sdl.cmd
+++ lib/copy_sdl.cmd
@@ -1,46 +1,81 @@
 @ECHO OFF
+SETLOCAL
+SET use_staticsdl=1
 
-ECHO Creating /lib Directory Structure ...
+IF EXIST ..\src\sdl2\VisualC\Win32\Release ^
+IF EXIST ..\src\sdl2\VisualC\x64\Release ^
+IF EXIST ..\src\sdl2\VisualC\Win32\Debug ^
+IF EXIST ..\src\sdl2\VisualC\x64\Debug ^
+IF EXIST "..\src\sdl2\VisualC\Win32\Debug [Static]" ^
+IF EXIST "..\src\sdl2\VisualC\x64\Debug [Static]" ^
+IF EXIST "..\src\sdl2\VisualC\Win32\Release [Static]" ^
+IF EXIST "..\src\sdl2\VisualC\x64\Release [Static]" (
+ GOTO choice
+) ELSE (
+ ECHO You must build SDL before running this!
+ PAUSE
+ GOTO:EOF
+)
 
-mkdir x86\Debug
-mkdir x86\Release
-mkdir x64\Debug
-mkdir x64\Release
-mkdir "x86\Debug [Static]"
-mkdir "x86\Release [Static]"
-mkdir "x64\Debug [Static]"
-mkdir "x64\Release [Static]"
+:choice
+set /P c=Do you want to use the static SDL libraries? (Y/N)
+IF /I "%c%" EQU "Y" (
+ SET use_staticsdl=1
+ GOTO start
+)
+IF /I "%c%" EQU "N" (
+ SET use_staticsdl=0
+ GOTO start
+)
+goto :choice
 
-ECHO Copying SDL2 library files ...
+:start
+ECHO Creating /lib directory structure ...
 
-xcopy ..\src\sdl2\VisualC\Win32\Debug\SDL2.lib x86\Debug\ /C /Q /Y 
-xcopy ..\src\sdl2\VisualC\Win32\Debug\SDL2main.lib x86\Debug\ /C /Q /Y 
-xcopy ..\src\sdl2\VisualC\Win32\Debug\SDL2.dll x86\Debug\ /C /Q /Y 
-xcopy ..\src\sdl2\VisualC\Win32\Debug\SDL2.pdb x86\Debug\ /C /Q /Y 
+if not exist x86\Debug mkdir x86\Debug
+if not exist x86\Release mkdir x86\Release
+if not exist x64\Debug mkdir x64\Debug
+if not exist x64\Release mkdir x64\Release
 
-xcopy ..\src\sdl2\VisualC\Win32\Release\SDL2.lib x86\Release\ /C /Q /Y 
-xcopy ..\src\sdl2\VisualC\Win32\Release\SDL2main.lib x86\Release\ /C /Q /Y 
-xcopy ..\src\sdl2\VisualC\Win32\Release\SDL2.dll x86\Release\ /C /Q /Y 
+IF %use_staticsdl%==1 (
+ ECHO Copying SDL2 static libraries...
+ xcopy "..\src\sdl2\VisualC\Win32\Debug [Static]\SDL2.lib" "x86\Debug\" /C /Q /Y
+ xcopy "..\src\sdl2\VisualC\Win32\Release [Static]\SDL2.lib" "x86\Release\" /C /Q /Y
+ xcopy "..\src\sdl2\VisualC\x64\Debug [Static]\SDL2.lib" "x64\Debug\" /C /Q /Y
+ xcopy "..\src\sdl2\VisualC\x64\Release [Static]\SDL2.lib" "x64\Release\" /C /Q /Y
+) ELSE (
+ ECHO Copying SDL2 shared libraries and files ...
+ 
+IF NOT EXIST ..\src\sdl2\VisualC\Win32\Release\SDL2.dll ^
+IF NOT EXIST ..\src\sdl2\VisualC\x64\Release\SDL2.dll ^
+IF NOT EXIST ..\src\sdl2\VisualC\Win32\Debug\SDL2.dll ^
+IF NOT EXIST ..\src\sdl2\VisualC\x64\Debug\SDL2.dll (
+ ECHO You need to build the shared libraries, they do not exist.
+ PAUSE
+ GOTO:EOF
+)
+ 
+ xcopy ..\src\sdl2\VisualC\Win32\Debug\SDL2.lib x86\Debug\ /C /Q /Y
+ xcopy ..\src\sdl2\VisualC\Win32\Debug\SDL2.dll x86\Debug\ /C /Q /Y
+ xcopy ..\src\sdl2\VisualC\Win32\Debug\SDL2.pdb x86\Debug\ /C /Q /Y
 
-xcopy ..\src\sdl2\VisualC\x64\Debug\SDL2.lib x64\Debug\ /C /Q /Y 
-xcopy ..\src\sdl2\VisualC\x64\Debug\SDL2main.lib x64\Debug\ /C /Q /Y 
-xcopy ..\src\sdl2\VisualC\x64\Debug\SDL2.dll x64\Debug\ /C /Q /Y 
-xcopy ..\src\sdl2\VisualC\x64\Debug\SDL2.pdb x64\Debug\ /C /Q /Y 
+ xcopy ..\src\sdl2\VisualC\Win32\Release\SDL2.lib x86\Release\ /C /Q /Y
+ xcopy ..\src\sdl2\VisualC\Win32\Release\SDL2.dll x86\Release\ /C /Q /Y
 
-xcopy ..\src\sdl2\VisualC\x64\Release\SDL2.lib x64\Release\ /C /Q /Y 
-xcopy ..\src\sdl2\VisualC\x64\Release\SDL2main.lib x64\Release\ /C /Q /Y 
-xcopy ..\src\sdl2\VisualC\x64\Release\SDL2.dll x64\Release\ /C /Q /Y 
+ xcopy ..\src\sdl2\VisualC\x64\Debug\SDL2.lib x64\Debug\ /C /Q /Y
+ xcopy ..\src\sdl2\VisualC\x64\Debug\SDL2.dll x64\Debug\ /C /Q /Y
+ xcopy ..\src\sdl2\VisualC\x64\Debug\SDL2.pdb x64\Debug\ /C /Q /Y
 
-xcopy "..\src\sdl2\VisualC\Win32\Debug [Static]\SDL2.lib" "x86\Debug [Static]\" /C /Q /Y 
-xcopy "..\src\sdl2\VisualC\Win32\Debug\SDL2main.lib" "x86\Debug [Static]\" /C /Q /Y 
+ xcopy ..\src\sdl2\VisualC\x64\Release\SDL2.lib x64\Release\ /C /Q /Y
+ xcopy ..\src\sdl2\VisualC\x64\Release\SDL2.dll x64\Release\ /C /Q /Y
+)
 
-xcopy "..\src\sdl2\VisualC\Win32\Release [Static]\SDL2.lib" "x86\Release [Static]\" /C /Q /Y 
-xcopy "..\src\sdl2\VisualC\Win32\Release\SDL2main.lib" "x86\Release [Static]\" /C /Q /Y 
+ECHO Copying SDL2main libraries ...
 
-xcopy "..\src\sdl2\VisualC\x64\Debug [Static]\SDL2.lib" "x64\Debug [Static]\" /C /Q /Y 
-xcopy "..\src\sdl2\VisualC\x64\Debug\SDL2main.lib" "x64\Debug [Static]\" /C /Q /Y 
+xcopy ..\src\sdl2\VisualC\Win32\Debug\SDL2main.lib x86\Debug\ /C /Q /Y
+xcopy ..\src\sdl2\VisualC\Win32\Release\SDL2main.lib x86\Release\ /C /Q /Y
+xcopy ..\src\sdl2\VisualC\x64\Debug\SDL2main.lib x64\Debug\ /C /Q /Y
+xcopy ..\src\sdl2\VisualC\x64\Release\SDL2main.lib x64\Release\ /C /Q /Y
 
-xcopy "..\src\sdl2\VisualC\x64\Release [Static]\SDL2.lib" "x64\Release [Static]\" /C /Q /Y 
-xcopy "..\src\sdl2\VisualC\x64\Release\SDL2main.lib" "x64\Release [Static]\" /C /Q /Y
-
+:end
 ECHO Done.
