28d69e9701a0231f66ec319a39b6ec22c86d67c3
John Tullos <xeek@xeekworx.com>
2018-01-28 15:42:10 -0600

    Cleaner main for the demo application

--- src/demo/main.cpp
+++ src/demo/main.cpp
@@ -1,13 +1,12 @@
 #include "demo_application.h"
+#include <memory>
 
 int main(int argc, char *argv[])
 {
-	demo_application * app = new demo_application();
-
-	int exitcode = app->run(argc, argv);
-
-	delete app;
-	app = nullptr;
-
+    int exitcode = 0;
+    {
+        std::unique_ptr<demo_application> app = std::make_unique<demo_application>();
+        exitcode = app->run(argc, argv);
+    }
 	return exitcode;
 }
